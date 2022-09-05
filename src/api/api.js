fee79e09f9d389340840699552a9a5fbc549cdcd
SnailHead <Held2001@mail.ru>
2022-09-05 16:32:22 +0400

    All api and one thunk

---
+++ src/api/api.js
@@ -0,0 +1,31 @@
+import axios from "axios";
+
+export const GetServices = async (searchQuery) => {
+    const response = await axios.post("https://plugin-medok.spaceapp.ru/ApiConsultations/Specializations/GetSpecializations",
+        {"RequestData": {TitleSearchString: searchQuery}});
+    return response.data.ResponseData.ListOfSpecializations;
+}
+
+export const GetDoctors = async (specializationId) => {
+    const response = await axios.post("https://plugin-medok.spaceapp.ru/ApiConsultations/Doctors/GetDoctorsBySpecializationId",
+        {RequestData: {SpecializationId: specializationId}});
+    return response.data.ResponseData.ListOfDoctors;
+}
+
+export const GetDoctorVisitType = async (specializationId, doctorId) => {
+    const response = await axios.post("https://plugin-medok.spaceapp.ru/ApiConsultations/Doctors/GetDoctorsTypeOfVisits",
+        {RequestData: {SpecializationId: specializationId, DoctorId: doctorId}});
+    return response.data.ResponseData.ListOfTypes;
+}
+
+export const GetDoctorsConsultationsByMainType = async (specializationId, doctorId, consultationMainType) => {
+    const response = await axios.post("https://plugin-medok.spaceapp.ru/ApiConsultations/Consultations/GetDoctorsConsultationsByMainType",
+        {RequestData: {SpecializationId: specializationId, DoctorId: doctorId, ConsultationMainType:consultationMainType}});
+    return response.data.ResponseData.Consultations;
+}
+
+export const CreateMessageForConsultation = async (request) => {
+    const response = await axios.post("https://plugin-medok.spaceapp.ru/ApiConsultations/Service/CreateMessageForConsultation",
+        {RequestData: request});
+    return response.data.ResponseData.Consultations;
+}
--- src/store/slice/serviceSlice.js
+++ src/store/slice/serviceSlice.js
@@ -1,6 +1,7 @@
 import { createSlice } from "@reduxjs/toolkit";
 import travm1 from "../../assets/img/special1.png";
 import travm2 from "../../assets/img/special2.png";
+import {GetServices} from "../../api/api.js";
 const initialState = {
   service: [
     {
@@ -53,7 +54,21 @@ const initialState = {
 export const serviceSlice = createSlice({
   name: "service",
   initialState,
-  reducers: {},
+  reducers: {
+    GetServices(state, action) {
+      state.service = action.payload;
+    },
+  },
 });
+
+export const GetServicesPost = (
+    searchQuery
+) => {
+  return async (dispatch) => {
+    const response = await GetServices(searchQuery);
+    dispatch(serviceSlice.actions.GetServices(response));
+  };
+};
+
 
 export default serviceSlice.reducer;
